62852c9b067a965e6483d61c360b9dd40173122f
Glitch (hadesbotty-testy) <none>
2018-02-15 21:25:19 +0000

    table on searchtech

--- commands/searchtech.js
+++ commands/searchtech.js
@@ -5,28 +5,36 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
     
   const techID = args[0];
   if (!client.config.hadesTech[techID]) return message.reply(`Invalid Tech: ${techID}`);
-  
-  
+    
   const table = require('easy-table');
   var hasData=false;
   var techTable = new table;
-
-    
-  var msg = `Searching for: ${client.config.hadesTech[techID].desc}`;
-
-  client.hsTech.forEach(function (allTech, userID, mapObj) { 
-    if (allTech["#Guild"] == message.guild.id){
-      const techLevel = allTech[techID];
-      if (techLevel >0) {
-        msg += (`\n<@${userID}>: ${techLevel}`);
-          techTable.cell('Time', );
-          techTable.cell('User', userData.userName);
-          techTable.newRow();
-      }
-    }
-});  
   
-  return message.reply(msg);
+  message.guild.members.forEach(function (user, userID, mapObj){
+    client.logger.debug("trying: "+userID);
+    if (client.hsTech.has(userID)) {
+      const allTech = client.hsTech.get(userID);
+      const techLevel = allTech[techID] || 0;
+      if (techLevel >0) {
+        //var userData = client.usersData.get(userID);
+        hasData=true;
+        techTable.cell('Level', techLevel);
+        techTable.cell('User', client.usersData.get(userID).userName);
+        techTable.newRow();
+      }
+      else client.logger.debug("Not tech for "+userID);
+    }
+    else client.logger.debug("No data for "+userID);
+
+  });
+
+  if (!hasData)
+    return message.reply("No data found");
+  else {
+    techTable.sort('Level');
+    return message.reply(`Searching for: ${client.config.hadesTech[techID].desc}\n` + techTable.toString());
+  }
+
 };
 
 exports.conf = {
