93086a52e0e46989dffd1fa90e66a2021b465aa7
Glitch (hadesbotty) <none>
2018-02-19 18:54:38 +0000

    🙅👡 export command

--- commands/export.js
+++ commands/export.js
@@ -12,7 +12,8 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
   var hasData=false,
       dataTable = new table,
       searchObj = message.guild,
-      dataObj = {};
+      dataObj = {},
+      html = "";
     
   // ************************************* return members of a specified ROLE
   if (args[0] === "role") {
@@ -21,12 +22,49 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
     searchObj = message.guild.roles.get(roleID);
   }
   
-  //sort thru all users in a guild or role and export only the ones that have data on client.hsTech  
+  var targetMembers = [];
+  //var targetMembers = {};
+  searchObj.members.forEach(function (target, targetID, mapObj){
+    targetMembers.push(targetID);
+    //targetMembers[targetID] = mapObj.username;
+    //client.logger.debug("::"+targetID+":::"+mapObj.username);
+  }); 
+  
+  // Make Header
+  html = "<HTML><BODY><TABLE><TR><TH>User</TH>";
+  Object.keys(client.config.hadesTech).forEach(techID => {html += "<TH>"+techID+"</TH>";});
+  html += "</TR>";  
+
+  client.hsTech.forEach(function (target, targetID, mapObj){
+    if (targetMembers.includes(targetID)) {
+    //if (targetMembers.hasOwnProperty(targetID)) {
+      hasData=true;
+      //var targetDB = client.userDB.get(targetID) || {username: targetMembers[targetID]}
+      var targetDB = client.userDB.get(targetID) || {username: targetID}
+      
+      html += "<TR><TD>"+targetDB.username+"</TD>";
+      Object.keys(client.config.hadesTech).forEach(techID => { 
+        var techLevel = client.hsTech.get(targetID)[techID] || 0;
+        html += "<TD>"+techLevel+"</TD>";
+      });
+      html += "</TR>";   
+    }
+  });
+
+  // close the html
+  html += "</TABLE></BODY></HTML>";
+  
+/*  
+  //sort thru all users in a guild or role and export only the ones that have data on client.hsTech 
   searchObj.members.forEach(function (target, targetID, mapObj){
     if (client.hsTech.has(targetID)) {
       hasData=true;
       var allTech = client.hsTech.get(targetID);
       var targetDB = client.userDB.get(targetID) || {username: target}
+      
+      Object.keys(allTech).map(function(techID, index) {
+        //client.logger.log("x");
+      });
 
       //attempt with TABLEIFY
       //all wrong... https://hadesbotty.glitch.me/TABLEIFY_exaple_output.html
@@ -38,18 +76,18 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
       Object.keys(allTech).map(function(techID, index) {
         dataTable.cell(techID, allTech[techID]);
       });
-      dataTable.newRow();
-      
-    }
-  });  
+      dataTable.newRow();   
+   }
+  }); 
+*/     
+    
   if (!hasData) return message.reply("No data found");
   
-  client.logger.debug(JSON.stringify(dataObj));
-    
   var filename = searchObj.id+".html";
   
-  fs.writeFile(dir+filename, dataTable.sort('User').toString(), function(err) {  //attempt with EASY-TABLE
+  //fs.writeFile(dir+filename, dataTable.sort('User').toString(), function(err) {  //attempt with EASY-TABLE
   //fs.writeFile(dir+filename, tableify(dataTable), function(err) {              //attempt with TABLEIFY
+  fs.writeFile(dir+filename, html, function(err) {
     if(err) {
       return message.reply(`Erro salvando arquivo...`+dir+filename);
       client.logger.error(err);
@@ -57,14 +95,14 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
     client.logger.log("The file was saved: "+dir+filename);
   }); 
   return message.reply(`Data exported for ${args[0]||"guild"} ${searchObj.name}:\n Check it on: `+url+filename); 
-  
+ 
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
   aliases: [],
-  permLevel: "Bot Owner"
+  permLevel: "Administrator"
 };
 
 exports.help = {
