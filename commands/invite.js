4f4e99d7a901b734c3d7e15fa309965d5f3028c7
Glitch (hadestestty) <none>
2018-03-26 16:44:53 +0000

    including AlphaDrone

---
+++ commands/invite.js
@@ -0,0 +1,27 @@
+exports.run = async (client, message, args, level) => { 
+  
+  message.channel.send({embed: {
+    color: 0x00AE86,
+    title: "So you like me, huh ? Where do you wanna take me to ?",
+    url: process.env.BOT_INVITE_LINK,
+    description: "Use the link above to invite me to a new Corp. Server",
+    footer: {
+      text: "PS: need to be a server admin or owner..."
+    }
+  }});
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "invite",
+  category: "Miscelaneous",
+  description: "Get a link to the invite URL.",
+  usage: "invite"
+};
