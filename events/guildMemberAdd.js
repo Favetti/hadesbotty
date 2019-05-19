20d6fa35731ad498e2fddae46570998532a50268
Glitch (bonez565-hadesbotty) <none>
2019-05-19 09:47:50 +0000

    Split large messages

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -8,7 +8,7 @@ module.exports = (client, member) => {
   if (settings.welcomeEnabled !== "true") return;
 
   // Replace the placeholders in the welcome message with actual data
-  const welcomeMessage = settings.welcomeMessage.replace("{{user}}", member.user.tag);
+  const welcomeMessage = settings.welcomeMessage.replace("{{user}}", member.user);
 
   // Send the welcome message to the welcome channel
   // There's a place for more configs here.
